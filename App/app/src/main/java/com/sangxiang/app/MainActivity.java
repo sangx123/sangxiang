e115bf3cd597c41ed9122fa4fbff8d4b93b366a7
sangxiang <xiang.sang@mykaishi.com>
2017-04-20 13:32:56 +0800

    添加javaTest项

--- App/app/src/main/java/com/sangxiang/app/MainActivity.java
+++ App/app/src/main/java/com/sangxiang/app/MainActivity.java
@@ -16,12 +16,10 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import java.util.ArrayList;
 
 
-
-
 public class MainActivity extends LogActivity {
 
     RecyclerView mRecyclerView;
-    private ArrayList<String> date=new ArrayList<>();
+    private ArrayList<String> date = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,32 +28,33 @@ public class MainActivity extends LogActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
 
 
-        for (int i=0;i<100;i++){
-            date.add("我是第"+i+"个");
+        for (int i = 0; i < 100; i++) {
+            date.add("我是第" + i + "个");
         }
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_main,date) {
+        mRecyclerView.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.adapter_main, date) {
                                      @Override
                                      protected void convert(BaseViewHolder helper, String item) {
-                                         helper.setText(R.id.mTxt,item);
+                                         helper.setText(R.id.mTxt, item);
                                      }
                                  }
         );
         mRecyclerView.addItemDecoration(new MyItemDecoration());
     }
 
-    public class MyItemDecoration extends RecyclerView.ItemDecoration{
+    public class MyItemDecoration extends RecyclerView.ItemDecoration {
 
 
         private final Paint dividerPaint;
-        private int mTopHeight=30;
+        private int mTopHeight = 100;
 
-        public MyItemDecoration(){
+        public MyItemDecoration() {
             dividerPaint = new Paint();
             dividerPaint.setColor(Color.RED);
             dividerPaint.setStrokeWidth(10);
             dividerPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         }
+
         @Override
         public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
             super.onDraw(c, parent, state);
@@ -88,20 +87,34 @@ public class MainActivity extends LogActivity {
         public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
             super.onDrawOver(c, parent, state);
 
-            int bottom = 0;
+            int right = parent.getWidth();
+            //得到第一个可见item的位置
+            int index = ((LinearLayoutManager) (parent.getLayoutManager())).findFirstVisibleItemPosition();
+            //出现一个奇怪的bug，有时候child为空，所以将 child = parent.getChildAt(i)。-》 parent.findViewHolderForLayoutPosition(index).itemView
+            View child = parent.findViewHolderForLayoutPosition(index).itemView;
+            //第一个类型的Item 的 header 在 position==2（firstItemPosition）处，如果index==2，则说明此item在顶部
+            //index>=2，说明之后的item都有吸顶效果
+            Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+            int top = parent.getPaddingTop();
+            int bottom = parent.getPaddingTop() + mTopHeight;
+            //制造"最新"（下一组第一个item）和"推荐"交接时，被顶上去的效果
+            //在还没开始交接的时候，"最新"的getBottom()>bottom
             if (isFirstOfGroup(index + 1)) {
-                // 下一个组马上到达顶部
-                bottom = Math.min(child.getBottom(), mTopHeight);
-            } else {
-                // 普通情况
-                bottom = mTopHeight;
+                bottom = Math.min(child.getBottom(), bottom);
             }
-
-
+            c.drawRect(0, top, right, bottom, paint);
+            /*paint.setColor(Color.BLACK);
+            paint.setTextSize(60);
+            paint.setTextAlign(Paint.Align.LEFT);
+            paint.setAntiAlias(true);
+            c.drawText(tag, left, bottom, paint);*/
         }
+
         private boolean isFirstOfGroup(int index) {
             return index % 7 == 0;
         }
+
         private String getGroupName(int index) {
             return "第" + (index / 7 + 1) + "组";
         }
---
+++ App/testjava/src/main/java/com/example/TestJava.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class TestJava {
+    public static void main(String[] args){
+        System.out.print("111");
+    }
+}
